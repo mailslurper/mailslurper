67a0a300181206ec609adb0fd82ba95243101d57
Adam Presley <adam@adampresley.com>
2016-02-25 21:53:32 -0600

    Updated version number. Global DB handle

--- global/Global.go
+++ global/Global.go
@@ -1,6 +1,10 @@
 package global
 
+import "github.com/mailslurper/libmailslurper/storage"
+
 const (
 	// Version of the MailSlurper Server application
-	SERVER_VERSION string = "1.0.0"
+	SERVER_VERSION string = "1.8"
 )
+
+var Database storage.IStorage

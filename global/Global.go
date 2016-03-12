529a0697a96a4cdac5e872d54072b90c4a0ae454
Adam Presley <adam@adampresley.com>
2016-03-11 21:44:12 -0600

    Debug mode off by default

--- global/Global.go
+++ global/Global.go
@@ -9,7 +9,7 @@ import "github.com/mailslurper/libmailslurper/storage"
 const (
 	// Version of the MailSlurper Server application
 	SERVER_VERSION string = "1.9"
-	DEBUG_ASSETS   bool   = true
+	DEBUG_ASSETS   bool   = false
 )
 
 var Database storage.IStorage

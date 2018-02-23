e6b156df0d7473ae95d0641bb51dcacbdc9c3a4a
Adam Presley <adam@adampresley.com>
2018-02-23 17:08:56 -0600

    Test project for persisten connections

---
+++ cmd/DotNetPersistentConnection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace DotNetPersistentConnection {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("MailSlurper persistent connection test from .NET");
+
+			SmtpClient client = new SmtpClient("localhost", 2500);
+
+			client.Send(new MailMessage(
+				"test@test.com",
+				"test@test.com",
+				"Persistent Connection Test .NET",
+				"This is a test message. This tests how persistent connections work from a .NET client"
+			));
+		}
+	}
+}
